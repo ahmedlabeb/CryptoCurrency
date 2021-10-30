4c15fb15e6404324be335ab488013613ea98f709
ahmedlabeb <ahmed.m.labeb@gmail.com>
2021-10-30 16:27:57 +0400

    Update CryptoServiceError.java

--- src/main/java/com/bitoasis/cryptocurrencyservice/api/control/execption/CryptoServiceError.java
+++ src/main/java/com/bitoasis/cryptocurrencyservice/api/control/execption/CryptoServiceError.java
@@ -1,12 +1,4 @@
-/*
- * Copyright 2021 Mondia Media Group GmbH. All rights reserved.
- *
- * This software is the confidential and proprietary information
- * of Mondia Media Group GmbH ("Confidential Information").
- * You shall not disclose such Confidential Information and shall
- * use it only in accordance with the terms of the license agreement
- * you entered into with Mondia Media Group GmbH.
- */
+
 package com.bitoasis.cryptocurrencyservice.api.control.execption;
 
 import org.springframework.http.HttpStatus;
